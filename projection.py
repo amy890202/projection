e2ffe2f450e6bbfa476a3c5088118f316d313bd9
amy890202 <52898990+amy890202@users.noreply.github.com>
2021-12-02 08:04:18 +0800

    Add files via upload

---
+++ projection.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 17 19:21:33 2021
+
+@author: user
+"""
+import numpy as np
+#xy是相框
+x1 = 159
+y1 = 56
+x2 =388
+y2 = 81
+x3 = 159
+y3 = 396
+x4 = 387
+y4 = 380
+
+#xy是相框裡的
+p1 = 0
+q1=0
+p2=453
+q2=0
+p3=0
+q3=453
+p4=453
+q4=453
+
+A=np.zeros((8,8))
+b = np.zeros((8,1))
+A[0,:] = [x1,y1,1,0,0,0,-p1*x1,-p1*y1]
+A[1,:] = [x2,y2,1,0,0,0,-p2*x2,-p2*y2]
+A[2,:] = [x3,y3,1,0,0,0,-p3*x3,-p3*y3]
+A[3,:]=[x4,y4,1,0,0,0,-p4*x4,-p4*y4]
+A[4,:]= [0,0,0,x1,y1,1,-q1*x1,-q1*y1]
+A[5,:]= [0,0,0,x2,y2,1,-q2*x2,-q2*y2]
+A[6,:]= [0,0,0,x3,y3,1,-q3*x3,-q3*y3]
+A[7,:]= [0,0,0,x4,y4,1,-q4*x4,-q4*y4]
+b[:,0] = [p1,p2,p3,p4,q1,q2,q3,q4]
+
+x = np.linalg.lstsq(A,b)[0]
+
+H= np.zeros((3,3))
+H[0,0] = x[0]
+H[0,1] = x[1]
+H[0,2] = x[2]
+H[1,0] = x[3]
+H[1,1] = x[4]
+H[1,2] = x[5]
+H[2,0] = x[6]
+H[2,1] = x[7]
+H[2,2] = 1
+
+print(H)
+from PIL import Image
+from scipy import signal
+
+BG = Image.open('BG.jpg')
+BG_data = np.asarray(BG).copy()
+I = Image.open('snape.jpg')
+data = np.asarray(I)
+[H1,W1,t] = BG_data.shape
+[H2,W2,t] = data.shape
+
+xy1 = np.ones((3,1))
+pq1 = np.ones((3,1))
+for h in range(H1):
+    for w in range(W1):
+        xy1[0] = w
+        xy1[1] = h
+        pq1 = np.dot(H,xy1)
+        p = int(pq1[0][0]/pq1[2][0])
+        q = int(pq1[1][0]/pq1[2][0])
+        if(p>=0 and p<W2 and q>=0 and q<H2):
+            BG_data[h,w,:]= data[q,p,:]
+            #print(h,w,p,q)
+            
+I2 = Image.fromarray(BG_data)
+I2.show()
+        
